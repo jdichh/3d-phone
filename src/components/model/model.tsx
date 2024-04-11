af54a1c008530592a11ed124b09d045fb01e7c47
jdichh <jpsdichoso21@gmail.com>
2024-04-12 07:13:41 +0800

    feat: init model view config

--- src/components/model/model.tsx
+++ src/components/model/model.tsx
@@ -1,10 +1,10 @@
 import { useGSAP } from "@gsap/react";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { yellowImg } from "@/lib/media";
 import gsap from "gsap";
 import SectionHeading from "../sectionheading/sectionheading";
 import ModelView from "./modelview";
-import { yellowImg } from "@/lib/media";
-
+import * as THREE from "three";
 
 const Model = () => {
   useGSAP(() => {
@@ -14,24 +14,49 @@ const Model = () => {
     });
   }, []);
 
-  const [phoneSize, setPhoneSize] = useState('small')
+  const [phoneSize, setPhoneSize] = useState("small");
   const [model, setModel] = useState({
-    title: 'phone in X color',
+    title: "phone in X color",
     color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
     img: yellowImg,
-  })
+  });
 
-  // 3js camera controls
+  // 3js camera control tracker
   const cameraControlSmallPhone = useRef();
   const cameraControlBigPhone = useRef();
+
+  // models & model config tracker
+  const smallPhone = useRef(new THREE.Group());
+  const bigPhone = useRef(new THREE.Group());
+
+  // model rotation state
+  const [smallPhoneRotation, setSmallPhoneRotation] = useState(0);
+  const [bigPhoneRotation, setBigPhoneRotation] = useState(0);
 
   return (
     <section className="common-padding">
       <div className="screen-width">
         <SectionHeading heading="Take a closer look." />
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-            <ModelView />
+          <div className="w-full h-[75vh] md:h-[85vh] overflow-hidden relative outline">
+            <ModelView
+              index={1}
+              groupRef={smallPhone}
+              gsapType="view1"
+              controlRef={cameraControlSmallPhone}
+              setRotationValue={setSmallPhoneRotation}
+              item={model}
+              size={phoneSize}
+            />
+            <ModelView
+              index={2}
+              groupRef={bigPhone}
+              gsapType="view2"
+              controlRef={cameraControlBigPhone}
+              setRotationValue={setBigPhoneRotation}
+              item={model}
+              size={phoneSize}
+            />
           </div>
         </div>
       </div>
