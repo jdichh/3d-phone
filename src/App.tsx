40a1a61e9c34d23188939973cdd576a61e75b572
jdichh <jpsdichoso21@gmail.com>
2024-04-11 10:51:40 +0800

    feat: initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Hero from "./components/hero/hero";
+import Highlights from "./components/highlights/highlights";
+import NavBar from "./components/navbar/navbar";
+
 const App = () => {
   return (
-    <div className="text-2xl">App</div>
-  )
-}
+    <div className="bg-black">
+      <NavBar />
+      <Hero />
+      <Highlights />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,7 @@
+const Hero = () => {
+  return (
+    <div>hero</div>
+  )
+}
+
+export default Hero
