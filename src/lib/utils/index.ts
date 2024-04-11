99b1c901d3d0f8b5068eec003ad2836b304834d1
jdichh <jpsdichoso21@gmail.com>
2024-04-11 10:43:30 +0800

    chore: img/video im/ex/ports

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,57 @@
+import hero from "/assets/images/hero.webp";
+
+import hmv from "/assets/videos/hero.webm";
+import smallmv from "/assets/videos/smallHero.webm";
+import highlightFirst from "/assets/videos/highlight1.webm";
+import highlightSec from "/assets/videos/highlight2.webm";
+import highlightThird from "/assets/videos/highlight3.webm";
+import highlightFourth from "/assets/videos/highlight4.webm";
+import exploremv from "/assets/videos/explore.webm";
+import framemv from "/assets/videos/frame.webm";
+
+import apple from "/assets/images/apple.svg";
+import search from "/assets/images/search.svg";
+import bag from "/assets/images/bag.svg";
+import watch from "/assets/images/watch.svg";
+import right from "/assets/images/right.svg";
+import replay from "/assets/images/replay.svg";
+import play from "/assets/images/play.svg";
+import pause from "/assets/images/pause.svg";
+
+import yellow from "/assets/images/yellow.webp";
+import blue from "/assets/images/blue.webp";
+import white from "/assets/images/white.webp";
+import black from "/assets/images/black.webp";
+import explore1 from "/assets/images/explore1.webp";
+import explore2 from "/assets/images/explore2.webp";
+import chip from "/assets/images/chip.webp";
+import frame from "/assets/images/frame.png";
+
+export const heroImg = hero;
+
+export const heroVideo = hmv;
+export const smallHeroVideo = smallmv;
+export const highlightFirstVideo = highlightFirst;
+export const highlightSecondVideo = highlightSec;
+export const highlightThirdVideo = highlightThird;
+export const highlightFourthVideo = highlightFourth;
+export const exploreVideo = exploremv;
+export const frameVideo = framemv;
+
+export const appleImg = apple;
+export const searchImg = search;
+export const bagImg = bag;
+export const watchImg = watch;
+export const rightImg = right;
+export const replayImg = replay;
+export const playImg = play;
+export const pauseImg = pause;
+
+export const yellowImg = yellow;
+export const blueImg = blue;
+export const whiteImg = white;
+export const blackImg = black;
+export const explore1Img = explore1;
+export const explore2Img = explore2;
+export const chipImg = chip;
+export const frameImg = frame;
