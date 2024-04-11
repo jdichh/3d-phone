e5ae6b5ac2a8d4a1ac0ecd90a30ff042f1694c77
jdichh <jpsdichoso21@gmail.com>
2024-04-11 15:33:10 +0800

    feat: initial carousel

--- src/components/videocarousel/videocarousel.tsx
+++ src/components/videocarousel/videocarousel.tsx
@@ -1,7 +1,37 @@
+import { featuresSlides } from "../../lib/constants";
 const VideoCarousel = () => {
-  return (
-    <div>videocarousel</div>
-  )
-}
 
-export default VideoCarousel
+  return (
+    <>
+      <div className="flex items-center">
+        {featuresSlides.map((feature) => (
+          <div key={feature.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-2xl overflow-hidden bg-black">
+                <video
+                  id="video"
+                  playsInline={true}
+                  preload="auto"
+                  muted
+                  loop={true}
+                  autoPlay={true}
+                >
+                  <source src={feature.video} type="video/webm" />
+                </video>
+              </div>
+              <div className="absolute top-6 left-6 z-10">
+                {feature.textLists.map((text) => (
+                  <p key={text} className="text-lg md:text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default VideoCarousel;
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,14 @@
+import {
+  blackImg,
+  blueImg,
+  highlightFirstVideo,
+  highlightFourthVideo,
+  highlightSecondVideo,
+  highlightThirdVideo,
+  whiteImg,
+  yellowImg,
+} from "../utils/index";
+
 export const navLinks = [
   {
     link: "https://jdichh.vercel.app",
@@ -7,4 +18,66 @@ export const navLinks = [
     link: "https://www.linkedin.com/in/jason-dichoso/",
     name: "LinkedIn"
   },
+];
+
+export const featuresSlides = [
+  {
+    id: 1,
+    textLists: [
+      "The Apple A17 Pro.",
+      "A game‑changing chip.",
+      "Groundbreaking performance.",
+    ],
+    video: highlightFirstVideo,
+  },
+  {
+    id: 2,
+    textLists: [
+      "iPhone 15 Pro Max has the",
+      "longest optical zoom in iPhone history, ever.",
+    ],
+    video: highlightSecondVideo,
+  },
+  {
+    id: 3,
+    textLists: ["Titanium.", "So strong. So light. So Pro."],
+    video: highlightThirdVideo,
+  },
+  {
+    id: 4,
+    textLists: ["All-new Action button.", "What will yours do?."],
+    video: highlightFourthVideo,
+  },
+];
+
+export const models = [
+  {
+    id: 1,
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
+    img: yellowImg,
+  },
+  {
+    id: 2,
+    title: "iPhone 15 Pro in Blue Titanium",
+    color: ["#53596E", "#6395ff", "#21242e"],
+    img: blueImg,
+  },
+  {
+    id: 3,
+    title: "iPhone 15 Pro in White Titanium",
+    color: ["#C9C8C2", "#ffffff", "#C9C8C2"],
+    img: whiteImg,
+  },
+  {
+    id: 4,
+    title: "iPhone 15 Pro in Black Titanium",
+    color: ["#454749", "#3b3b3b", "#181819"],
+    img: blackImg,
+  },
+];
+
+export const sizes = [
+  { label: '6.1"', value: "small" },
+  { label: '6.7"', value: "large" },
 ];
